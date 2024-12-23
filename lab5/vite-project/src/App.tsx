f59043979d3a689d260cf3927a3abd134d97b59f
jansarba <jansarbasarba@gmail.com>
2024-12-23 12:02:16 +0100

    przygotowanie do lab5

---
+++ lab5/vite-project/src/App.tsx
@@ -0,0 +1,9 @@
+import Welcome from './components/Welcome.tsx'
+function App() {
+
+  return (
+    <Welcome name='Sarah'/>
+  )
+}
+
+export default App
---
+++ lab5/vite-project/src/components/Welcome.tsx
@@ -0,0 +1,10 @@
+interface WelcomeProps {
+    name: string;
+}
+
+function Welcome(props: WelcomeProps) {
+    return <h1>Hello, {props.name}</h1>;
+    
+}
+
+export default Welcome
